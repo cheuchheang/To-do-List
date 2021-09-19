b5fca471ef0de028a2eab1f5dd8df453dc48188c
searcheuchheang <searcheuchheang@gmail.com>
2021-09-19 17:50:43 +0700

    modify again

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 document.getElementById("todo").innerHTML=localStorage.getItem("todo")
 function inputTask(){
-    event.preventDefault()
-
-    let tasks=[]
-    localStorage.setItem("todo",tasks.toString())
-    let getFromStorage=localStorage.getItem("todo").split("")
     let getValue=document.getElementsByTagName("input")[0].value
-    let time=new Date().toString()
-    getFromStorage.push(`<li>${getValue}<span>${time}</span></li>`)
-   
-     let getValueTotal=document.getElementsByTagName('ul')[0].innerHTML+=`<li>${getValue}<span>${time}</span></li>`
+    let date=document.getElementById('date').value
+    let time=document.getElementById('time').value
+     let getValueTotal=document.getElementsByTagName('ul')[0].innerHTML+=`<li>${getValue}<span>${date} ${time}</span></li>`
         localStorage.setItem("todo",getValueTotal)
     document.getElementsByTagName("form")[0].reset()
     
