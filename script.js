66a4bb84411315c6836eda17e087949f3b97f12e
searcheuchheang <searcheuchheang@gmail.com>
2021-09-19 16:06:52 +0700

    Finish all

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-let tasks=[]
-
-
+document.getElementById("todo").innerHTML=localStorage.getItem("todo")
 function inputTask(){
     event.preventDefault()
+
+    let tasks=[]
+    localStorage.setItem("todo",tasks.toString())
+    let getFromStorage=localStorage.getItem("todo").split("")
     let getValue=document.getElementsByTagName("input")[0].value
     let time=new Date().toString()
-    tasks.push(`<li>${getValue}<span>${time}</span></li>`)
-    
-        document.getElementsByTagName('ul')[0].innerHTML+=`<li>${getValue}<span>${time}</span></li>`
- 
-    console.log(tasks);
+    getFromStorage.push(`<li>${getValue}<span>${time}</span></li>`)
+   
+     let getValueTotal=document.getElementsByTagName('ul')[0].innerHTML+=`<li>${getValue}<span>${time}</span></li>`
+        localStorage.setItem("todo",getValueTotal)
     document.getElementsByTagName("form")[0].reset()
     
+}
+function clearTask() {
+  
+    localStorage.clear()
+    location.reload()
+ 
 }
